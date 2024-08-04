e8f59cf972fdf58695b30f0fb1184722b0ed431c
Hassan Ahmed <hassanshakeel381@gmail.com>
2024-08-04 23:40:42 +0500

    firestore modified

--- home.js
+++ home.js
@@ -18,13 +18,147 @@ import {
   const form = document.querySelector('#form')
   const todo = document.querySelector('#todo')
   const ul   = document.querySelector('#ul')
-  const select   = document.querySelector('#select')
+  const select  = document.querySelector('#select')
   const reset = document.querySelector('.reset') 
   const citiesBtn   = document.querySelectorAll('.cities-btn')
     
 
-  let arr []
+
+  let arr = [];    
+
+
+  
+  
+  //cities 
+
+  
+  citiesBtn.forEach((btn) => {
+    btn.addEventListener('click', async (event) => {
+      let arr = [] ;
+      console.log(event.target.innerHTML);
+      const todosRef = collection(db , "todos")
+      
+      try {
+        const q = query(
+          todosRef,
+          where("city", "==", event.target.innerHTML ),
+          orderBy("time", "desc")
+        )
+        
+        const querySnapshot = await getDocs(q)
+        querySnapshot.forEach((doc) => {
+        arr.push({...doc.data() , id: doc.id})
+      })
+      console.log(arr);
+      renderTodo();
+      
+      } catch (error) {
+        console.error("Error getting document : " , error);
+      }    
+      console.log(arr);
+      renderTodo();
+      
+    })
+  })
+  
+  reset.addEventListener('click' , getData)
+  
+  
+  
+  
+  // const description = document.querySelector('#description')
+  
+  async function getData(){
+    arr = []
+    const q = query(collection(db, 'todos'), orderBy('time', 'desc'))
+    const querySnapshot = await getDocs(q);
+    querySnapshot.forEach((doc) => {
+    arr.push({...doc.data(), id: doc.id})  
+  });
+  console.log(arr);
+  renderTodo()
+}
+
+getData( )
+
+//render data on screen
+
+function renderTodo() {
+  ul.innerHTML = "";
+  if (arr.lenght === 0) {
+    ul.innerHTML = "no data found";
+    return;
+  }
+  arr.map((item) => {
+    ul.innerHTML +=`
+    <li>${item.todo}
+    <button class="deleteBtn">Delete</button>
+    <button class="editBtn">Edit</button>
+    </li>
+    <p>
+    ${item.time ? item.time.toDate() : "no time"}
+    </p>
+    <hr />
+    `;
+  })
+
+  
+  
+const deleteBtn = document.querySelectorAll('.deleteBtn')
+const editBtn = document.querySelectorAll('.editBtn')
+
+deleteBtn.forEach((btn , index) => {
+  btn.addEventListener('click' , async () => {
+    console.log(arr[index]);
+    await deleteDoc(doc(db, 'todos' , arr[index].id))
+    console.log("data deleted");
+    arr.splice(index , 1)
+    renderTodo()  
+  })
+})
+
+editBtn.forEach((btn , index) => {
+  btn.addEventListener('click' , async () => {
+    const updatedNewValue = prompt('enter new value')
+    const todoUpdate = doc(db , 'todos' , arr[index].id)
+    await updateDoc(todoUpdate, {
+      todo: updatedNewValue,
+    });
+    console.log('Data Updated');
+    arr[index].todo = updatedNewValue
+    renderTodo();
     
+  })
+})
+}
+
+
+
+// Add todo in Database
+
+form.addEventListener('submit', async (event)=>{
+  event.preventDefault();
+
+  try {
+    const docRef = await addDoc(collection(db, "todos"), 
+    {
+      todo: todo.value,
+      city: select.value,
+      time: Timestamp.fromDate(new Date()), 
+    });
+    console.log("Document written with ID: ", docRef.id);
+    arr.push({
+      todo : todo.value,
+      id: docRef.id,
+      city: select.value,
+    });
+    renderTodo();
+    todo.value = " " ;
+  } catch (e) {
+    console.error("Error adding document: ", e);
+  }
+})
+
 
 
 onAuthStateChanged(auth, (user) => {
@@ -47,97 +181,3 @@ onAuthStateChanged(auth, (user) => {
           console.log(error);
         });
   })
-
-
-  //cities 
-
-  citiesBtn.forEach((btn) => {
-    btn.addEventListener('click', async (event) =>{
-      arr =[]
-      console.log(event.target.innerHTML);
-      const todoRef = collection(db , "todos")
-      const querySnapshot= query(
-        todoRef,
-        where("city", " ==", event.target.innerHTML ),
-        orderBy("time", "desc")
-      )
-      const querySnapshot = await getDocs(q)
-      querySnapshot.forEach((doc) =>{
-        arr.push({...doc.data() , id: doc.id})
-      })
-      console.log(arr);
-      renderTodo()
-      
-    })
-  })
-  
-  reset.addEventListener('click' , getData)
-
-
-
-
-// const description = document.querySelector('#description')
-
-let arr = [];
-
-async function getData(){
-    const arr = []
-    const querySnapshot = await getDocs(collection(db, "users"));
-    querySnapshot.forEach((doc) => {
-    arr.push(doc.data())  
-  });
-  console.log(arr);
-}
-
-getData( )
-
-//render data on screen
-
-function renderTodo() {
-  ul.innerHTML = "";
-  if (arr.lenght === 0) {
-    ul.innerHTML = "no data found";
-    return;
-  }
-  arr.map((item) =>{
-    ul.innerHTML +=`
-      <li>${item.code}
-        <button class="delete-btn">Delete</button>
-        <button class="edit-btn">Edit</button>
-      </li>
-      <p>
-      ${item.time ? item.time.toDate() : "no time"}
-      </p>
-      <hr>
-      `;
-  })
-}
-
-
-// Add todo in Database
-
-form.addEventListener('submit', async (event)=>{
-  event.preventDefault();
-  // console.log(todo.value);
-  // console.log(description.value);
-
-  try {
-    const docRef = await addDoc(collection(db, "users"), 
-    {
-      todo: todo.value,
-      // description: description.value,
-      // uid:auth.currentUser.uid
-    });
-    console.log("Document written with ID: ", docRef.id);
-    arr.push({
-      todo : todo.value,
-      id: docRef.id,
-    })
-    renderTodo();
-    todo.value = " " ;
-  } catch (e) {
-    console.error("Error adding document: ", e);
-  }
-})
-
-
